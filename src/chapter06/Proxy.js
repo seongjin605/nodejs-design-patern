9fce3d6f81d79e0500db5280892041115b33c704
preciouStar <qkrtjdwls605@naver.com>
2021-04-06 09:07:18 +0900

    AOP or function hook

---
+++ src/chapter06/Proxy.js
@@ -0,0 +1,10 @@
+const scientist = {
+  name: 'nikola',
+  surname: 'tesla'
+};
+
+const uppercaseScientist = new Proxy(scientist, {
+  get: (target, property) => target[property].toUpperCase()
+});
+
+console.log(uppercaseScientist.name, uppercaseScientist.surname);
